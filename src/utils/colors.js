190183331fed004876b54b44379f2418ce995bbf
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-11-10 17:25:42 +0100

    refactor: add lightGray color

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -4,3 +4,5 @@ export const blue = '#3B82F6';
 export const red = '#EF4444';
 export const white = '#fff';
 export const black = '#000';
+export const lightGray = '#D1D5DB';
+export const gray = '#9CA3AF';
