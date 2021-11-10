89244e4c11d1c748acc96d6adc6118722555ebd3
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-11-10 12:02:33 +0100

    update: add color utils

---
+++ src/utils/colors.js
@@ -0,0 +1,6 @@
+export const green = '#10B981';
+export const yellow = '#F59E0B';
+export const blue = '#3B82F6';
+export const red = '#EF4444';
+export const white = '#fff';
+export const black = '#000';
