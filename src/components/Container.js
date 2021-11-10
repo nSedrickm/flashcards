493e51e28dbfe472dde80eff2f545efa1f7a0e89
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-11-10 12:00:54 +0100

    update: add container component

---
+++ src/components/Container.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, View, Dimensions } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { green, blue } from '../utils';
+
+const width = Dimensions.get('window').width; //full width
+const height = Dimensions.get('window').height; //full height
+
+export const Container = ({ children }) => {
+    return (
+        <LinearGradient
+            colors={[blue, green]}
+            style={styles.container}
+        >
+            {children}
+        </LinearGradient>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        height: height,
+        width: width,
+    }
+});
