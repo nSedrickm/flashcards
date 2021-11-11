0e1ee21ef0086c0abb2da8212f9455310052c5d1
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-11-11 15:43:08 +0100

    update: add 404 for decks

--- src/screens/DeckList.js
+++ src/screens/DeckList.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Container, DeckCard } from '../components';
-import { FlatList } from 'react-native';
+import { FlatList, StyleSheet, View, Text } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { getDecks } from '../services/api';
+import { white } from '../utils';
 
 const DeckList = ({ navigation }) => {
 
@@ -18,20 +20,47 @@ const DeckList = ({ navigation }) => {
 
     return (
         <Container>
-            <FlatList
-                data={decks}
-                keyExtractor={item => item.title}
-                renderItem={({ item }) => (
-                    <DeckCard
-                        title={item.title}
-                        cards={item.cards}
-                        onPress={() => viewDetails(item.title)}
-                    />
-                )}
-            />
+            {decks.length ? (
+                <FlatList
+                    data={decks}
+                    keyExtractor={item => item.title}
+                    renderItem={({ item }) => (
+                        <DeckCard
+                            title={item.title}
+                            cards={item.cards}
+                            onPress={() => viewDetails(item.title)}
+                        />
+                    )}
+                />) : (
+                <View style={styles.container}>
+                    <MaterialCommunityIcons name="cards-outline" size={128} color={white} />
+                    <Text style={styles.heading}>No decks</Text>
+                    <Text style={styles.description}>Add One to get started</Text>
+                </View>
+            )}
         </Container>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderRadius: 20,
+        borderColor: white,
+        height: 300
+    },
+    heading: {
+        color: white,
+        fontSize: 36,
+        fontWeight: 'bold'
+    },
+    description: {
+        color: white,
+    },
+})
 
 
 export default DeckList
